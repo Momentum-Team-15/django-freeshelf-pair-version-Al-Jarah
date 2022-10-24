0e128600c20fc2c7121cd89b4a85923f50f51e97
Elijah Brown <elibrown0731@gmail.com>
2022-10-24 16:05:36 -0400

    created  a view

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
-    path('', views.index, name="home"),
+    path('', views.resource_list, name="home"),
 ]
