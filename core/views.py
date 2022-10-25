2e4cbf5b650c127085a92ae49fce5aed82d39c59
Elijah Brown <elibrown0731@gmail.com>
2022-10-25 11:33:40 -0400

    adding form

--- core/views.py
+++ core/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import NameForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
-def resource_list(request):
-    return render(request, 'core/resource_list.html', {})
+def home(request):
+    user=request.user
+    return render(request, 'core/home.html', {})
+    
+def get_name(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+    else: 
+        form = NameForm()
+
+    return render(request, 'core/registration_form.html', {'form': form})
