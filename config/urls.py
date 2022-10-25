5c7d3dcc2eca96299fef39372cc8a2f122d1e13e
Elijah Brown <elibrown0731@gmail.com>
2022-10-25 11:32:16 -0400

    added views.home

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
-    path('', views.resource_list, name="home"),
+    path('', views.home, name="home"),
 ]
