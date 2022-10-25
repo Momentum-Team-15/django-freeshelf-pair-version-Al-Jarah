d87a7cb957b4b37feff8073cd7e25b755d349c44
Elijah Brown <elibrown0731@gmail.com>
2022-10-25 15:17:44 -0400

    turned self function into a f string and added a author

--- core/models.py
+++ core/models.py
@@ -8,17 +8,17 @@ class User(AbstractUser):
     pass
 
 class Resources(models.Model):
-    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
     
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
-    def __str__(self):
-        return self.title
 
