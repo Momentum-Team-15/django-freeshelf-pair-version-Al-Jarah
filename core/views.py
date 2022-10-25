1da078617858f81da2a248164d3fdb9ddb6246bd
Elijah Brown <elibrown0731@gmail.com>
2022-10-25 15:19:18 -0400

    added an edit function

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Resources
 from . forms import NameForm
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
@@ -15,3 +17,16 @@ def get_name(request):
         form = NameForm()
 
     return render(request, 'core/registration_form.html', {'form': form})
+
+def edit(request, pk):
+    resources = get_object_or_404(Resources, pk=pk)
+    if request.method == "POST":
+        form = NameForm(request.POST, request.FILES, instance=resources)
+        if form.is_valid():
+            form = form.save()
+            form.user = request.user
+            form.created_at = timezone.now()
+            return redirect('home', pk=form.pk)
+    else:
+        form = NameForm(instance=resources)
+    return render(request, 'core/edit.html', {'form': form})
